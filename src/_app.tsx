8605c39e80a7af5e318c2840fbb7e14e7483bf3b
Vasco Oliveira <vascooliveira@outlook.com>
2021-09-16 04:19:02 +0100

    feat(layout): setup MUI, add GlobalStyles and theme

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+import GlobalStyles from './styles/GlobalStyles';
 import Dashboard from './components/Dashboard';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#36498f',
+      light: '#2d7c9d',
+      dark: '#161d20',
+    },
+    secondary: {
+      main: '#a4a29e',
+      light: '#cccccc',
+    },
+  }
+});
 
 const App = () => {
   return (
-    <Dashboard />
+    <MuiThemeProvider theme={theme}>
+      <Dashboard />
+      <GlobalStyles />
+    </MuiThemeProvider>
   );
 }
 

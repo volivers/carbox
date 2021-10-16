9acc3f54abac91d349055bd8164b98300b6a1acc
Vasco Oliveira <vascooliveira@outlook.com>
2021-10-16 18:09:34 +0100

    fix: correct server side listener

--- server/server.ts
+++ server/server.ts
@@ -5,8 +5,4 @@ require('dotenv').config();
 const app = express();
 const PORT : string|number = process.env.PORT || 5000;
 
-app.use("*",(req, res) =>{
-    res.send("Server is up and running!");
-});
-
 app.listen(PORT,() => console.log(`hosting @${PORT}`));

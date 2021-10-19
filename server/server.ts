f091d66a74119c4db38421272466abdb7b3a8b0e
Vasco Oliveira <vascooliveira@outlook.com>
2021-10-19 13:29:27 +0100

    Revert "fix: correct server side listener"
    This reverts commit 9acc3f54abac91d349055bd8164b98300b6a1acc.

--- server/server.ts
+++ server/server.ts
@@ -5,4 +5,8 @@ require('dotenv').config();
 const app = express();
 const PORT : string|number = process.env.PORT || 5000;
 
+app.use("*",(req, res) =>{
+    res.send("Server is up and running!");
+});
+
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
